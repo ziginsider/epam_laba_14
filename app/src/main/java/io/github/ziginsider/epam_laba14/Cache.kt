7661174922dbe2ca5506e1d8a9901d5be45f789e
ziginsider <ziginsider@gmail.com>
2018-05-14 20:00:24 +0300

    add LRU cache interface

---
+++ app/src/main/java/io/github/ziginsider/epam_laba14/Cache.kt
@@ -0,0 +1,48 @@
+package io.github.ziginsider.epam_laba14
+
+/**
+ * A LRU Cache interface
+ *
+ * @author Alex Kisel
+ * @since 2018-05-03
+ */
+interface Cache<in K, V> {
+    /**
+     * Puts an value in the cache for [key]
+     *
+     * @param key key (url)
+     * @param value value (image)
+     * @return true if operation was successful, false - otherwise
+     */
+    fun put(key: K, value: V): Boolean
+
+    /**
+     * Get the value for the [key]
+     *
+     * @param key key (url)
+     * @return value (image) for the specified [key]
+     */
+    fun get(key: K): V
+
+    /**
+     * Sets the size of the cache
+     *
+     * @param maxSize new size of the cache
+     * @return true if operation was successful, false - otherwise
+     */
+    fun resize(maxSize: Int): Boolean
+
+    /**
+     * Returns the current memory size of the cache
+     *
+     * @return current memory size of the cache
+     */
+    fun getCurrentSize(): Int
+
+    /**
+     * Returns the max memory size of the cache
+     *
+     * @return max memory size of the cache
+     */
+    fun getMaxSize(): Int
+}
