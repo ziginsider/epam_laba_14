eb4ca107df5710b9b6cf607eec181bb53a466174
ziginsider <ziginsider@gmail.com>
2018-05-19 23:31:45 +0300

    add contract for flickr

---
+++ app/src/main/java/io/github/ziginsider/epam_laba14/retrofit/Contract.kt
@@ -0,0 +1,9 @@
+package io.github.ziginsider.epam_laba14.retrofit
+
+object Contract {
+    const val key = "23d9808055fcf4f6e5671f40eb926bcc"
+    const val method = "flickr.photos.getRecent"
+    const val format = "json"
+    const val raw = 1
+    const val extras = "url_c" //url_m ??
+}
