265d3e76ac95576b5cbce3a9d36d436393c7552a
ziginsider <ziginsider@gmail.com>
2018-05-19 23:22:56 +0300

    configure retrofit service flickr

--- app/src/main/java/io/github/ziginsider/epam_laba14/retrofit/RetrofitService.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba14/retrofit/RetrofitService.kt
@@ -1,17 +1,33 @@
 package io.github.ziginsider.epam_laba14.retrofit
 
-import okhttp3.ResponseBody
-import retrofit2.Call
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Url
+import retrofit2.http.Query
 
 /**
- * Declaration a Retrofit interface for image downloading
+ * Declaration a Retrofit interface for getting recent photos from flickr
  *
  * @since 2018-04-10
  * @author Alex Kisel
  */
 interface RetrofitService {
-    @GET
-    fun downloadImage(@Url url: String): Call<ResponseBody>
+
+    @GET("/services/rest")
+    fun recentPhotos(@Query("method") url: String,
+                     @Query("api_key") key: String,
+                     @Query("format") format: String,
+                     @Query("nojsoncallback") raw: Int,
+                     @Query("extras") extras: String): Observable<RecentPhotos>
+
+    companion object {
+        fun create(): RetrofitService {
+            val retrofit = Retrofit.Builder()
+                    .baseUrl("https://api.flickr.com/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            return retrofit.create(RetrofitService::class.java)
+        }
+    }
 }
