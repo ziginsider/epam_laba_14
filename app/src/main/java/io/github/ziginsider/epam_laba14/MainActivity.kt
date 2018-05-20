e8dfc543b966799641d08c8ef5ebe9f7ea81d68d
ziginsider <ziginsider@gmail.com>
2018-05-20 15:05:40 +0300

    first demo RecyclerView from flickr prework

--- app/src/main/java/io/github/ziginsider/epam_laba14/MainActivity.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba14/MainActivity.kt
@@ -2,9 +2,19 @@ package io.github.ziginsider.epam_laba14
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.view.View
+import io.github.ziginsider.epam_laba14.adapter.RecyclerViewAdapter
+import io.github.ziginsider.epam_laba14.model.Photo
+import io.github.ziginsider.epam_laba14.retrofit.Contract.API_KEY
+import io.github.ziginsider.epam_laba14.retrofit.Contract.FORMAT
+import io.github.ziginsider.epam_laba14.retrofit.Contract.JSON_RAW
+import io.github.ziginsider.epam_laba14.retrofit.Contract.METHOD
+import io.github.ziginsider.epam_laba14.retrofit.Contract.URL_TYPE
 import io.github.ziginsider.epam_laba14.retrofit.RetrofitService
 import io.github.ziginsider.epam_laba14.utils.logi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -31,25 +41,32 @@ class MainActivity : AppCompatActivity() {
 
 
         progressBar.visibility = View.VISIBLE
-        RetrofitService.create().recentPhotos()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        RetrofitService.create()
+                .recentPhotos(METHOD, API_KEY, FORMAT, 10, 1, JSON_RAW, URL_TYPE)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    response -> response.photos.data
+                    progressBar.visibility = View.GONE
+                }, { error ->
+                    error.printStackTrace()
+                })
 
         logi(TAG, "[ COUNT THREADS = ${ImageLoader.threadCount} ]")
         logi(TAG, "[ COUNT CAPACITY CACHE = ${ImageLoader.cacheCapacity} ]")
         logi(TAG, "[ SIZE CACHE = ${ImageLoader.cacheSize} ]")
+    }
+
+    private fun addPhotos(photos: List<Photo>) {
+        val recyclerAdapter = RecyclerViewAdapter(R.layout.item_view, {})
+
+        with (recyclerView) {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            setHasFixedSize(true)
+            adapter = recyclerAdapter
+        }
+
+        recyclerAdapter.submitList(photos)
+
     }
 }
