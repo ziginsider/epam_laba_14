f4b44c30b55d279c6454c59395989a7a346c29f0
ziginsider <ziginsider@gmail.com>
2018-05-20 13:46:27 +0300

    add RecyclerView template

--- app/src/main/java/io/github/ziginsider/epam_laba14/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba14/adapter/RecyclerViewAdapter.kt
@@ -1,12 +1,24 @@
 package io.github.ziginsider.epam_laba14.adapter
 
+import android.support.v7.recyclerview.extensions.ListAdapter
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
 import io.github.ziginsider.epam_laba14.model.Photo
+import kotlinx.android.exten
 
-class RecyclerViewAdapter() { //TODO ListAdapter
+class RecyclerViewAdapter(private val layoutResId: Int, private val clickListener: (Photo) -> Unit)
+    : ListAdapter<Photo, ViewHolder>(PhotoDiffCallback()) {
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): {
 
-    //TODO CreateViewHolder setOnClickListener
+    }
 
+    override fun onBindViewHolder(holder:, position: Int) {
 
-    //TODO ViewHolder implements LayoutContainer
+    }
+
+    class ViewHolder() : RecyclerView.ViewHolder(), LayoutContainer {
+        
+    }
+
 }
