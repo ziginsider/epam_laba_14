704cc24fa0a81b480325dd26e5575f76111e38a2
ziginsider <ziginsider@gmail.com>
2018-05-20 14:44:11 +0300

    add ProgressBar to layout

--- app/src/main/java/io/github/ziginsider/epam_laba14/MainActivity.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba14/MainActivity.kt
@@ -17,15 +17,32 @@ class MainActivity : AppCompatActivity() {
 
         logi(TAG, "[ COUNT PROCESSORS = $count ]")
 
-        ImageLoader.threadCount = 7
-        ImageLoader.cacheCapacity = 15
+//        ImageLoader.threadCount = 7
+//        ImageLoader.cacheCapacity = 15
         //ImageLoader.cacheSize = 1024 * 1024 * 4
 
-        ImageLoader.displayImage(imageView, "http://www.oiseaux.net/photos/robert.balestra/images/pic.epeiche.roba.3p.jpg")
-        ImageLoader.displayImage(imageView2, "http://www.realbirder.com/EcuadorD/Blue-grayTanagerEasternSp.JPG")
-        ImageLoader.displayImage(imageView3, "http://www.oiseaux.net/photos/alain.chappuis/images/chardonneret.elegant.alch.11g.jpg")
-        ImageLoader.displayImage(imageView4, "http://www.uroc5962.fr/wp-content/uploads/2018/04/BEBOPpoitiers123309451399_art.jpg")
-        ImageLoader.displayImage(imageView5, "http://www.oiseaux.net/photos/jules.fouarge/images/pic.epeiche.jufo.8g.jpg")
+//        ImageLoader.displayImage(imageView, "http://www.oiseaux.net/photos/robert.balestra/images/pic.epeiche.roba.3p.jpg")
+//        ImageLoader.displayImage(imageView2, "http://www.realbirder.com/EcuadorD/Blue-grayTanagerEasternSp.JPG")
+//        ImageLoader.displayImage(imageView3, "http://www.oiseaux.net/photos/alain.chappuis/images/chardonneret.elegant.alch.11g.jpg")
+//        ImageLoader.displayImage(imageView4, "http://www.uroc5962.fr/wp-content/uploads/2018/04/BEBOPpoitiers123309451399_art.jpg")
+//        ImageLoader.displayImage(imageView5, "http://www.oiseaux.net/photos/jules.fouarge/images/pic.epeiche.jufo.8g.jpg")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         logi(TAG, "[ COUNT THREADS = ${ImageLoader.threadCount} ]")
         logi(TAG, "[ COUNT CAPACITY CACHE = ${ImageLoader.cacheCapacity} ]")
