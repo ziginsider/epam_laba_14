fe3c9e8cf96f97fdc9d7b78a81bcd77d2b3ed9fd
ziginsider <ziginsider@gmail.com>
2018-05-20 14:08:40 +0300

    implements onCreateVieweweHolder and onBindViewHolder

--- app/src/main/java/io/github/ziginsider/epam_laba14/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba14/adapter/RecyclerViewAdapter.kt
@@ -5,17 +5,19 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import io.github.ziginsider.epam_laba14.model.Photo
+import io.github.ziginsider.epam_laba14.utils.inflate
 import kotlinx.android.extensions.LayoutContainer
 
 class RecyclerViewAdapter(private val layoutResId: Int, private val clickListener: (Photo) -> Unit)
     : ListAdapter<Photo, RecyclerViewAdapter.ViewHolder>(PhotoDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): {
-
+        val view = parent inflate layoutResId
+        return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder:, position: Int) {
-
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position), clickListener)
     }
 
     class ViewHolder(override val containerView: View?)
