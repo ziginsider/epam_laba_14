17b9500b9e541bcaa79e0ada51eaa73f071cc123
ziginsider <ziginsider@gmail.com>
2018-05-20 22:51:31 +0300

    add EndlessScrollListener

---
+++ app/src/main/java/io/github/ziginsider/epam_laba14/adapter/EndlessScrollListener.kt
@@ -0,0 +1,35 @@
+package io.github.ziginsider.epam_laba14.adapter
+
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+
+class EndlessScrollListener(val func: () -> Unit, val layoutManager: LinearLayoutManager)
+    : RecyclerView.OnScrollListener() {
+
+    private var previousTotal = 0
+    private var loading = true
+    private var visibleThreshold = 3
+    private var firstVisibleItem = 0
+    private var visibleItemCount = 0
+    private var totalItemCount = 0
+
+    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+        super.onScrolled(recyclerView, dx, dy)
+        if (dy > 0) {
+            visibleItemCount = recyclerView.childCount
+            totalItemCount = layoutManager.itemCount
+            firstVisibleItem = layoutManager.findFirstVisibleItemPosition()
+            if (loading) {
+                if (totalItemCount > previousTotal) {
+                    loading = false
+                    previousTotal = totalItemCount
+                }
+            }
+            if (!loading && (totalItemCount - visibleItemCount)
+                    <= (firstVisibleItem + visibleThreshold)) {
+                func()
+                loading = true
+            }
+        }
+    }
+}
